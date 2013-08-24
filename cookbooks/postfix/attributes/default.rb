61f79e7a42e22422afbafb7d7157547857bc84cb
Juraci Paixão Kröhling <jpkrohling@gmail.com>
2013-08-24 17:18:18 +0200

    Enforce usage of TLS by other servers when sending/receiving messages

--- cookbooks/postfix/attributes/default.rb
+++ cookbooks/postfix/attributes/default.rb
@@ -3,3 +3,10 @@ default['postfix']['smtpd_recipient_restrictions'] = 'permit_sasl_authenticated,
 default['postfix']['virtual_mailbox_domains'] = 'mysql:/etc/postfix/mysql-virtual-mailbox-domains.cf'
 default['postfix']['virtual_mailbox_maps'] = 'mysql:/etc/postfix/mysql-virtual-mailbox-maps.cf'
 default['postfix']['virtual_alias_maps'] = 'mysql:/etc/postfix/mysql-virtual-alias-maps.cf'
+
+# this enforces tls for sending/receiving emails, but some sites, like github, don't support sending emails
+# via starttls... so, disable it if you want better compatibility, and enable it for improved security
+default['postfix']['smtpd_enforce_tls'] = 'yes' # change to no for better compatibility
+default['postfix']['smtp_enforce_tls'] = node['postfix']['smtpd_enforce_tls']
+default['postfix']['smtpd_tls_security_level'] = 'encrypt' # change it to 'maybe' for better compatibility
+default['postfix']['smtp_tls_security_level'] = node['postfix']['smtpd_tls_security_level']
