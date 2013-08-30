fa0b97bfed0216198ef37f33f3491b7c0b51dcb7
Juraci Paixão Kröhling <jpkrohling@gmail.com>
2013-08-30 18:56:15 +0200

    Added a basic new role for code infra

---
+++ roles/code.rb
@@ -0,0 +1,39 @@
+name 'code'
+description 'Code-related services, like CI tool, repository, ...'
+run_list(
+		'recipe[hostname]',
+		'recipe[build-essential]',
+		'recipe[openssl]',
+		'recipe[x509]',
+		'recipe[chef-client]',
+		'recipe[base]',
+		'recipe[mariadb]',
+		'recipe[iptables]',
+		'recipe[apache]',
+		'recipe[newrelic]',
+)
+
+default_attributes(
+		:set_fqdn => '*.kroehling.de',
+		:x509 => {
+				:ca => 'kroehling.de',
+				:country => 'DE',
+				:state => 'Bayern',
+				:city => 'Muenchen',
+				:organization => 'kroehling.de',
+				:email => 'ops@kroehling.de'
+		},
+		:build_essential => {
+				:compiletime => true
+		},
+		:newrelic => {
+				:server_monitoring => {
+						:ssl => true,
+						# :license => 'REPLACE_WITH_YOUR_KEY' ## set it before running the newrelic recipe
+				},
+				:application_monitoring => {
+						:ssl => true,
+						# :license => 'REPLACE_WITH_YOUR_KEY' ## set it before running the newrelic recipe
+				}
+		},
+)
